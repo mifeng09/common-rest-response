99309d3eae1f5ac5795accb01be0044c26b45f32
GANG XU <GANG XU>
2016-09-06 14:47:59 +0800

    init

---
+++ src/main/java/com/cloume/common/rest/response/App.java
@@ -0,0 +1,13 @@
+package com.cloume.common.rest.response;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
